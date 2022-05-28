1212dd84744f8c84fb14c0ed321a29b7fdd249a5
Fred Monaquezi <fredmonaquezi@me.com>
2022-05-27 22:44:49 -0300

    file updated

---
+++ sandbox.js
@@ -0,0 +1,56 @@
+//1 - User will choose rock, paper or scissor
+//2 - computer will select one of them at random
+//3 - declare a winner, loser or draw
+//4 - make the function case senstive so no mistakes happen
+
+let playerSelection = prompt('Choose one', '');
+let playerScore = 0;
+let computerScore = 0;
+
+const myArray = ['rock', 'paper', 'scissors'];
+
+function computerPlay(computerSelection){
+    computerSelection = myArray[Math.floor(Math.random() * myArray.length)]
+
+    return computerSelection
+
+}
+
+let computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection){
+    // computerSelection = computerPlay().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    if(playerSelection == computerSelection){
+        return 'Tie!'
+    } else if(
+        (computerSelection == 'rock' && playerSelection == 'scissors') ||
+        (computerSelection == 'paper' && playerSelection == 'rock') ||(computerSelection == 'scissors' && playerSelection == 'paper')) {
+            computerScore++
+            if(computerScore >= 3){
+                return 'The computer wins the game';
+            } else
+           return 'The computer scores a point';
+        } else if (
+            (playerSelection == 'rock' && computerSelection == 'scissors') ||
+            (playerSelection == 'paper'&& computerSelection == 'rock')||
+            (playerSelection == 'scissors' && computerSelection == 'paper')){
+                playerScore++
+                if(playerScore >= 3){
+                    return 'You win the game'
+                } else
+                return 'You win the game!';
+            }
+
+}
+
+function game(){
+    for(let i = 0; i < 5; i++);
+
+}
+
+console.log(playerScore);
+console.log(computerScore);
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
